e8401430ae337af6adea635b2df219d4794e91c9
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-08-13 00:33:11 +0900

    feat: バリデーションのテストコード作成

---
+++ app/domains/models/user_test.go
@@ -0,0 +1,99 @@
+package models_test
+
+import (
+	"echo_sprint_planner/app/domains/models"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func TestUser_CreateValidate(t *testing.T) {
+	now := time.Now()
+	user_id := uuid.New()
+	password := "password"
+
+	type fields struct {
+		ID       *uuid.UUID
+		Name     string
+		Email    string
+		IsActive bool
+		Password *string
+		CreateAt *time.Time
+		UpdateAt *time.Time
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "正常系",
+			fields: fields{
+				ID:       &user_id,
+				Name:     "test",
+				Email:    "test@test.com",
+				IsActive: true,
+				Password: &password,
+				CreateAt: &now,
+				UpdateAt: &now,
+			},
+			wantErr: false,
+		},
+		{
+			name: "異常系:名前が空",
+			fields: fields{
+				ID:       &user_id,
+				Name:     "",
+				Email:    "test@test.com",
+				IsActive: true,
+				Password: &password,
+				CreateAt: &now,
+				UpdateAt: &now,
+			},
+			wantErr: true,
+		},
+		{
+			name: "異常系:名前が3文字未満",
+			fields: fields{
+				ID:       &user_id,
+				Name:     "te",
+				Email:    "test@test.com",
+				IsActive: true,
+				Password: &password,
+				CreateAt: &now,
+				UpdateAt: &now,
+			},
+			wantErr: true,
+		},
+		{
+			name: "異常系:名前が51文字以上",
+			fields: fields{
+				ID:       &user_id,
+				Name:     "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest",
+				Email:    "test@test.com",
+				IsActive: true,
+				Password: &password,
+				CreateAt: &now,
+				UpdateAt: &now,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &models.User{
+				ID:       tt.fields.ID,
+				Name:     tt.fields.Name,
+				Email:    tt.fields.Email,
+				IsActive: tt.fields.IsActive,
+				Password: tt.fields.Password,
+				CreateAt: tt.fields.CreateAt,
+				UpdateAt: tt.fields.UpdateAt,
+			}
+			if err := u.CreateValidate(); (err != nil) != tt.wantErr {
+				t.Errorf("User.CreateValidate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
